4cd1483d6528f70bb35269db6ac504128e3b5990
SILK Admin <admin@smartinfologiks.com>
2020-07-16 09:42:11 +0000

    First commit

---
+++ boot.php
@@ -0,0 +1,9 @@
+<?php
+$wPath=dirname(getWebPath(__FILE__))."/";
+//echo $wPath;
+?>
+<script>
+nicIconPath="<?=$wPath?>nicedit/nicEditorIcons.gif";
+</script>
+
+<script src='<?=$wPath?>nicedit/nicEdit.js' type='text/javascript' language='javascript'></script>
